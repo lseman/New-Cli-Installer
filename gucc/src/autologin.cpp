6f1e24d5b92690d5c3c79113504cb94938ca0a88
Laio Seman <laioseman@gmail.com>
2026-09-12 20:09:50 -0300

    gucc: propagate errors from all autologin commands
    Replace ignored exec_checked() return values with proper error propagation.
    All sed commands now check for failure and return early with an error.
    
    Also refactored GDM config into a loop over sed commands to reduce duplication.

--- gucc/src/autologin.cpp
+++ gucc/src/autologin.cpp
@@ -12,34 +12,47 @@ using namespace std::string_view_literals;
 namespace gucc::user {
 
 auto enable_autologin(std::string_view displaymanager, std::string_view username, std::string_view root_mountpoint) noexcept -> Result<void> {
-    // TODO(vnepogodin): check for failed commands
-
-    // enable autologin
     if (displaymanager == "gdm"sv) {
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^AutomaticLogin=*/AutomaticLogin={}/g' {}/etc/gdm/custom.conf"), username, root_mountpoint));
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^AutomaticLoginEnable=*/AutomaticLoginEnable=true/g' {}/etc/gdm/custom.conf"), root_mountpoint));
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^TimedLoginEnable=*/TimedLoginEnable=true/g' {}/etc/gdm/custom.conf"), root_mountpoint));
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^TimedLogin=*/TimedLoginEnable={}/g' {}/etc/gdm/custom.conf"), username, root_mountpoint));
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^TimedLoginDelay=*/TimedLoginDelay=0/g' {}/etc/gdm/custom.conf"), root_mountpoint));
-    } else if (displaymanager == "lightdm"sv) {
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^#autologin-user=/autologin-user={}/' {}/etc/lightdm/lightdm.conf"), username, root_mountpoint));
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^#autologin-user-timeout=0/autologin-user-timeout=0/' {}/etc/lightdm/lightdm.conf"), root_mountpoint));
-
-        // create autologin group
-        if (!user::create_group("autologin"sv, root_mountpoint, true)) {
-            return make_error(ErrorCode::SubprocessFailed, "Failed to create autologin group");
+        const auto conf = fmt::format(FMT_COMPILE("{}/etc/gdm/custom.conf"), root_mountpoint);
+        for (const auto& sed : {
+            fmt::format(FMT_COMPILE("sed -i 's/^AutomaticLogin=*/AutomaticLogin={}/g' {}"), username, conf),
+            fmt::format(FMT_COMPILE("sed -i 's/^AutomaticLoginEnable=*/AutomaticLoginEnable=true/g' {}"), conf),
+            fmt::format(FMT_COMPILE("sed -i 's/^TimedLoginEnable=*/TimedLoginEnable=true/g' {}"), conf),
+            fmt::format(FMT_COMPILE("sed -i 's/^TimedLogin=*/TimedLogin={}/g' {}"), username, conf),
+            fmt::format(FMT_COMPILE("sed -i 's/^TimedLoginDelay=*/TimedLoginDelay=0/g' {}"), conf),
+        }) {
+            if (!utils::exec_checked(sed)) {
+                return make_error(ErrorCode::SubprocessFailed, fmt::format("autologin: failed to configure GDM"));
+            }
         }
-        // add group to user
-        const auto& groupadd_cmd = fmt::format(FMT_COMPILE("gpasswd -a {} autologin"), username);
-        if (!utils::arch_chroot_checked(groupadd_cmd, root_mountpoint)) {
-            return make_error(ErrorCode::SubprocessFailed, fmt::format("Failed to add autologin group to user: {}", username));
+    } else if (displaymanager == "lightdm"sv) {
+        const auto conf = fmt::format(FMT_COMPILE("{}/etc/lightdm/lightdm.conf"), root_mountpoint);
+        if (!utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^#autologin-user=/autologin-user={}/' {}"), username, conf))) {
+            return make_error(ErrorCode::SubprocessFailed, "autologin: failed to configure LightDM");
+        }
+        if (!utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^#autologin-user-timeout=0/autologin-user-timeout=0/' {}"), conf))) {
+            return make_error(ErrorCode::SubprocessFailed, "autologin: failed to configure LightDM timeout");
+        }
+
+        if (auto res = create_group("autologin"sv, root_mountpoint, true); !res) return res;
+        if (!utils::arch_chroot_checked(fmt::format(FMT_COMPILE("gpasswd -a {} autologin"), username), root_mountpoint)) {
+            return make_error(ErrorCode::SubprocessFailed, fmt::format("autologin: failed to add user '{}' to autologin group", username));
         }
     } else if (displaymanager == "plasmalogin"sv) {
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^User=/User={}/g' {}/etc/plasmalogin.conf"), username, root_mountpoint));
+        const auto conf = fmt::format(FMT_COMPILE("{}/etc/plasmalogin.conf"), root_mountpoint);
+        if (!utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^User=/User={}/g' {}"), username, conf))) {
+            return make_error(ErrorCode::SubprocessFailed, "autologin: failed to configure Plasma Login");
+        }
     } else if (displaymanager == "sddm"sv) {
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^User=/User={}/g' {}/etc/sddm.conf"), username, root_mountpoint));
+        const auto conf = fmt::format(FMT_COMPILE("{}/etc/sddm.conf"), root_mountpoint);
+        if (!utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^User=/User={}/g' {}"), username, conf))) {
+            return make_error(ErrorCode::SubprocessFailed, "autologin: failed to configure SDDM");
+        }
     } else if (displaymanager == "lxdm"sv) {
-        utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^# autologin=dgod/autologin={}/g' {}/etc/lxdm/lxdm.conf"), username, root_mountpoint));
+        const auto conf = fmt::format(FMT_COMPILE("{}/etc/lxdm/lxdm.conf"), root_mountpoint);
+        if (!utils::exec_checked(fmt::format(FMT_COMPILE("sed -i 's/^# autologin=dgod/autologin={}/g' {}"), username, conf))) {
+            return make_error(ErrorCode::SubprocessFailed, "autologin: failed to configure LXDM");
+        }
     }
 
     return {};
